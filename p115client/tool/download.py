db20623f79f2b16a560332a625e3d38a9d30f424
chenyanggao <2339083510@qq.com>
2025-07-20 22:55:45 +0800

    update.1753023345

--- p115client/tool/download.py
+++ p115client/tool/download.py
@@ -884,6 +884,7 @@ def make_db(
     clean: bool = True, 
     replace: bool = True, 
     with_path: bool = True, 
+    with_event: bool = False, 
     batch_size: int = 3_000, 
     id_to_dirnode: None | EllipsisType | MutableMapping[int, tuple[str, int] | DirNode] = None, 
     app: str = "android", 
@@ -901,6 +902,7 @@ def make_db(
     clean: bool = True, 
     replace: bool = True, 
     with_path: bool = True, 
+    with_event: bool = False, 
     batch_size: int = 3_000, 
     id_to_dirnode: None | EllipsisType | MutableMapping[int, tuple[str, int] | DirNode] = None, 
     app: str = "android", 
@@ -917,6 +919,7 @@ def make_db(
     clean: bool = True, 
     replace: bool = True, 
     with_path: bool = True, 
+    with_event: bool = False, 
     batch_size: int = 3_000, 
     id_to_dirnode: None | EllipsisType | MutableMapping[int, tuple[str, int] | DirNode] = None, 
     app: str = "android", 
@@ -933,17 +936,22 @@ def make_db(
     :param clean: 是否清理旧数据
     :param replace: 遇到相同 id 时是否替换数据
     :param with_path: 拉取数据时是否需要 "path" 字段
+    :param with_event: 是否收集和文件系统变更（文件的增删改）有关的事件，数据保存在 ``event`` 表中
     :param batch_size: 每批写入数据库的最多条数
     :param id_to_dirnode: 字典，保存 id 到对应文件的 ``DirNode(name, parent_id)`` 命名元组的字典
     :param app: 使用指定 app（设备）的接口
     :param max_workers: 最大并发数，用户拉取目录树，但写入数据库仍然是单线程的（经过测试如此效率更高）
     :param async_: 是否异步
     :param request_kwargs: 其它请求参数
+
+    :return: 一些统计信息
     """
     if isinstance(client, (str, PathLike)):
         client = P115Client(client, check_for_relogin=True)
     init_sql = """\
+-- 修改日志模式为 WAL (write-ahead-log)
 PRAGMA journal_mode = WAL;
+
 -- 创建表
 CREATE TABLE IF NOT EXISTS data (
     id INTEGER NOT NULL PRIMARY KEY,   -- id
@@ -953,35 +961,96 @@ CREATE TABLE IF NOT EXISTS data (
     sha1 TEXT NOT NULL DEFAULT '',     -- 文件的 sha1 散列值
     size INTEGER NOT NULL DEFAULT 0,   -- 文件大小
     pickcode TEXT NOT NULL DEFAULT '', -- 提取码
-    is_dir INTEGER NOT NULL DEFAULT 0, -- 是否目录，值总是 0
+    is_dir INTEGER NOT NULL DEFAULT 0, -- 是否目录
     path TEXT NOT NULL DEFAULT '',     -- 路径
     top_id INTEGER NOT NULL DEFAULT 0, -- 最近一次拉取时顶层目录的 id
     extra BLOB DEFAULT NULL,           -- 其它信息
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- 创建时间
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- 更新时间
 );
--- 创建索引
-CREATE INDEX IF NOT EXISTS idx_data_sha1_size ON data(sha1, size);
-CREATE INDEX IF NOT EXISTS idx_data_uid ON data(user_id);
+
+-- 创建索引（一些索引需要时候自己加）
 CREATE INDEX IF NOT EXISTS idx_data_pid ON data(parent_id);
 CREATE INDEX IF NOT EXISTS idx_data_tid ON data(top_id);
-CREATE INDEX IF NOT EXISTS idx_data_name ON data(name);
-CREATE INDEX IF NOT EXISTS idx_data_path ON data(path);
 CREATE INDEX IF NOT EXISTS idx_data_utime ON data(updated_at);
--- 创建更新时触发器
-CREATE TRIGGER IF NOT EXISTS trg_data_update
-AFTER UPDATE ON data 
-FOR EACH ROW 
+-- CREATE INDEX IF NOT EXISTS idx_data_sha1_size ON data(sha1, size);
+-- CREATE INDEX IF NOT EXISTS idx_data_uid ON data(user_id);
+-- CREATE INDEX IF NOT EXISTS idx_data_name ON data(name);
+-- CREATE INDEX IF NOT EXISTS idx_data_path ON data(path);
+"""
+    if with_event:
+        init_sql += """
+-- event 表，只用于记录 data 表中与文件系统有关的增删改事件
+CREATE TABLE IF NOT EXISTS event (
+    _id INTEGER PRIMARY KEY AUTOINCREMENT, -- 主键
+    id INTEGER NOT NULL,               -- id
+    is_dir INTEGER NOT NULL DEFAULT 0, -- 是否目录
+    pid0 INTEGER NOT NULL DEFAULT -1,  -- 更新前的父目录 id
+    pid1 INTEGER NOT NULL DEFAULT -1,  -- 更新后的父目录 id
+    name0 TEXT NOT NULL DEFAULT "",    -- 更新前的名字
+    name1 TEXT NOT NULL DEFAULT "",    -- 更新后的名字
+    path0 TEXT NOT NULL DEFAULT "",    -- 更新前的路径
+    path1 TEXT NOT NULL DEFAULT "",    -- 更新后的路径
+    event TEXT NOT NULL DEFAULT "",    -- 事件名
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- 创建时间
+);
+
+-- data 表发生新增
+DROP TRIGGER IF EXISTS trg_data_insert;
+CREATE TRIGGER trg_data_insert
+AFTER INSERT ON data
+FOR EACH ROW
+BEGIN
+    INSERT INTO event(id, is_dir, pid1, name1, path1, event) 
+    VALUES (NEW.id, NEW.is_dir, NEW.parent_id, NEW.name, NEW.path, 'add');
+END;
+
+-- data 表发生删除
+DROP TRIGGER IF EXISTS trg_data_delete;
+CREATE TRIGGER trg_data_delete
+AFTER DELETE ON data
+FOR EACH ROW
+BEGIN
+    INSERT INTO event(id, is_dir, pid0, name0, path0, event) 
+    VALUES (OLD.id, OLD.is_dir, OLD.parent_id, OLD.name, OLD.path, 'remove');
+END;
+
+-- data 表发生更新
+DROP TRIGGER IF EXISTS trg_data_update;
+CREATE TRIGGER trg_data_update
+AFTER UPDATE ON data
+FOR EACH ROW
+BEGIN
+    UPDATE data SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
+    INSERT INTO event(id, is_dir, pid0, pid1, name0, name1, path0, path1, event)
+    SELECT
+        OLD.id, OLD.is_dir, OLD.parent_id, NEW.parent_id, OLD.name, NEW.name, OLD.path, NEW.path, 
+        CASE WHEN OLD.parent_id != NEW.parent_id THEN 
+            CASE WHEN OLD.name != NEW.name THEN 'move,rename' ELSE 'move' END
+        ELSE 'rename' END
+    WHERE
+        OLD.parent_id != NEW.parent_id OR OLD.name != NEW.name;
+END;"""
+    else:
+        init_sql += """
+DROP TRIGGER IF EXISTS trg_data_insert;
+DROP TRIGGER IF EXISTS trg_data_delete;
+
+-- data 表发生更新
+DROP TRIGGER IF EXISTS trg_data_update;
+CREATE TRIGGER trg_data_update
+AFTER UPDATE ON data
+FOR EACH ROW
 BEGIN
     UPDATE data SET updated_at = CURRENT_TIMESTAMP WHERE id = NEW.id;
 END;"""
     clean_sql = """\
-WITH ids(id) AS (
-    SELECT id FROM data WHERE parent_id = :top_id AND updated_at < :start_t
-    UNION ALL
-    SELECT data.id FROM ids JOIN data ON (ids.id = data.parent_id) WHERE updated_at < :start_t
-)
 DELETE FROM data WHERE id in (
+    WITH ids(id) AS (
+        SELECT id FROM data WHERE parent_id = :top_id AND updated_at < :start_t
+        UNION ALL
+        SELECT data.id FROM ids JOIN data ON (ids.id = data.parent_id) WHERE updated_at < :start_t
+    )
     SELECT id FROM ids
     UNION ALL
     SELECT id FROM data WHERE top_id = :top_id AND updated_at < :start_t
@@ -992,20 +1061,22 @@ DELETE FROM data WHERE id in (
         if replace:
             if with_path:
                 sql = """\
-INSERT INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id, path)
-ON CONFLICT (id) DO UPDATE SET id = old.id;"""
-            else:
-                sql = """\
-INSERT INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id)
-ON CONFLICT (id) DO UPDATE SET id = old.id;"""
-        elif with_path:
-            sql = """\
 INSERT OR REPLACE INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id, path)
 VALUES (:id, :parent_id, :user_id, :name, :sha1, :size, :pickcode, :is_dir, :top_id, :path);"""
-        else:
-            sql = """\
+            else:
+                sql = """\
 INSERT OR REPLACE INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id)
 VALUES (:id, :parent_id, :user_id, :name, :sha1, :size, :pickcode, :is_dir, :top_id);"""
+        elif with_path:
+            sql = """\
+INSERT INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id, path)
+VALUES (:id, :parent_id, :user_id, :name, :sha1, :size, :pickcode, :is_dir, :top_id, :path)
+ON CONFLICT(id) DO UPDATE SET id = excluded.id;"""
+        else:
+            sql = """\
+INSERT INTO data (id, parent_id, user_id, name, sha1, size, pickcode, is_dir, top_id)
+VALUES (:id, :parent_id, :user_id, :name, :sha1, :size, :pickcode, :is_dir, :top_id)
+ON CONFLICT (id) DO UPDATE SET id = excluded.id;"""
         for record in records:
             record["user_id"] = user_id
             record["top_id"] = cid
@@ -1182,6 +1253,8 @@ def make_strm(
     :param max_workers: 最大并发数，用户拉取目录树，但写入本地文件仍然是单线程的（经过测试如此效率更高）
     :param async_: 是否异步
     :param request_kwargs: 其它请求参数
+
+    :return: 一些统计信息
     """
     if isinstance(client, (str, PathLike)):
         client = P115Client(client, check_for_relogin=True)
